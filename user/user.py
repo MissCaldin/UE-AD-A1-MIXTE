1fee7ecb10cabc936806fd8a35ce716eb7ca696e
estelle <estelle@maison.fr>
2024-10-09 10:33:54 +0200

    implémentation dans user des fonctionnalités n'ayant besoin que du service movies

--- user/user.py
+++ user/user.py
@@ -7,10 +7,10 @@ from werkzeug.exceptions import NotFound
 # CALLING gRPC requests
 import grpc
 from concurrent import futures
-import booking_pb2
+"""import booking_pb2
 import booking_pb2_grpc
 import movie_pb2
-import movie_pb2_grpc
+import movie_pb2_grpc"""
 
 # CALLING GraphQL requests
 # todo to complete
@@ -22,6 +22,44 @@ HOST = '0.0.0.0'
 
 with open('{}/data/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
+
+@app.route("/", methods=['GET'])
+def home():
+   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
+@app.route("/movies", methods=['GET'])
+def movies():
+   url="http://127.0.0.1:3001/graphql"
+   body= """
+   {movies{
+      title
+      rating
+      director
+      id}
+   }"""
+   response = requests.post(url=url, json={"query":body})
+   return make_response(jsonify(response.json()["data"]), 200)
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def movie_by_id(movieid):
+   url=f"http://127.0.0.1:3001/graphql"
+   body="""
+   {
+      movie_with_id(_id: "%s") {
+         id
+         title
+         rating
+         director
+      }
+   }
+   """ % movieid
+   response1=requests.post(url, json={"query":body})
+   return make_response(jsonify(response1.json()["data"]), 200)
+   
+@app.route("/<string:id>", methods=["GET"])
+def home_user(id):
+   user = next((user for user in users if user['id'] == id), None)
+   return make_response(jsonify(user['name']))
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
